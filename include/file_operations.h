2cdb9729df9214fbce27f2b5426f1717d9d651b6
emrekizilelma <emrekizilelma00@gmail.com>
2024-11-03 12:45:59 +0300

    added functions names

---
+++ include/file_operations.h
@@ -0,0 +1,9 @@
+#ifndef FILE_OPERATIONS_H
+#define FILE_OPERATIONS_H
+
+void create_file(void);
+void delete_file(void);
+void read_to_file(void); 
+void write_to_file(void); 
+
+#endif
---
+++ src/file_operations.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void create_file() {
+
+}
+
+void delete_file() {
+
+}
+
+void read_to_file() {
+
+}
+
+void write_to_file() {
+
+}
